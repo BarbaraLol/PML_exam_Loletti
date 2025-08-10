f93384fad2a6ca5447b26bcc2fcf6c80a985b156
BarbaraLol <barbara.loletti@gmail.com>
2025-08-10 22:27:29 +0200

    orfeo stuff

--- BCNN/sbatch.sh
+++ BCNN/sbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=BayesianTrain       # Job name
+#SBATCH --job-name=BCNN                # Job name
 #SBATCH --output=logs/%x_%j.out        # Stdout (%x=job-name, %j=job-id)
 #SBATCH --error=logs/%x_%j.err         # Stderr
 #SBATCH --partition=GPU                # GPU partition
@@ -36,8 +36,8 @@ python -c "import torch; print(f'PyTorch: {torch.__version__}'); \
            if torch.cuda.is_available(): print(f'GPU: {torch.cuda.get_device_name(0)}')"
 
 # --- Run your training script ---
-python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Registrazioni/audio_segments/ --batch_size 16 --output_dir results/20sec_chunks
+# python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Registrazioni/audio_segments/ --batch_size 16 --output_dir results/20sec_chunks
 # python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_5sec/audio_segments/ --batch_size 16 --output_dir results/5sec_chunks
-# python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_10sec/audio_segments/ --batch_size 16 --output_dir results/10sec_chunks
+python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_10sec/audio_segments/ --batch_size 16 --output_dir results/10sec_chunks
 
 
