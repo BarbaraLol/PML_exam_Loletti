f774cbaecc8c0f83a85bb48afa4d0d0e786bdaaa
BarbaraLol <barbara.loletti@gmail.com>
2025-08-11 11:35:03 +0200

    new orfeo training session

--- VAE/sbatch.sh
+++ VAE/sbatch.sh
@@ -20,7 +20,7 @@ module load cuda/12.1
 source ~/myenv/bin/activate
 # --- Run your training script ---
 # python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Registrazioni/audio_segments --batch_size 16 --output_dir vae_results/20sec_chunks --patience 5
-python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_5sec/audio_segments --batch_size 16 --output_dir vae_results/5sec_chunks --patience 5
-# python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_10sec/audio_segments --batch_size 16 --output_dir vae_results/10sec_chunks --patience 5
+# python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_5sec/audio_segments --batch_size 16 --output_dir vae_results/5sec_chunks --patience 5
+python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_10sec/audio_segments --batch_size 16 --output_dir vae_results/10sec_chunks --patience 5
 
 deactivate
