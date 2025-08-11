a7a6ab5a5bab30d982c15108a355bb908b60c7fd
BarbaraLol <147236478+BarbaraLol@users.noreply.github.com>
2025-08-11 10:53:10 +0200

    Delete bash.sh

--- bash.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get remove --purge '^nvidia-.*' '^cuda-.*' -y
-sudo apt-get autoremove -y
-sudo apt-get autoclean
-sudo rm -f /etc/apt/sources.list.d/cuda-*.list
-sudo rm -f /etc/apt/preferences.d/cuda-*
-sudo apt-get update
-sudo add-apt-repository ppa:graphics-drivers/ppa -y
-sudo apt-get update
-sudo apt-get install nvidia-driver-550 -y
