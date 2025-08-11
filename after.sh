85327d2b29b4fe98b6c4c041f72bdbc258df3719
BarbaraLol <147236478+BarbaraLol@users.noreply.github.com>
2025-08-11 10:52:55 +0200

    Delete after.sh

--- after.sh
+++
@@ -1,9 +0,0 @@
-nvidia-smi
-sudo apt-get install nvidia-cuda-toolkit -y
-nvcc --version
-python3 - <<EOF
-import torch
-print("CUDA available:", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("Device:", torch.cuda.get_device_name(0))
-EOF
