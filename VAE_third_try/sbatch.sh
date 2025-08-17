84c3eb03b11bbd8e624352b66b0902a6659f68b7
BarbaraLol <barbara.loletti@gmail.com>
2025-08-17 15:52:46 +0200

    orfeo

--- VAE_third_try/sbatch.sh
+++ VAE_third_try/sbatch.sh
@@ -23,7 +23,7 @@ source ~/myenv/bin/activate
 # python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_5sec/audio_segments --batch_size 16 --output_dir vae_results/5sec_chunks --patience 5
 # python3 train.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_10sec/audio_segments --batch_size 16 --output_dir vae_results/10sec_chunks --patience 5
 # python3 train2.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_10sec/audio_segments --batch_size 16 --output_dir vae_results/10sec_chunks --patience 5
-python3 train2.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_10sec/audio_segments --batch_size 16 --output_dir ./results
+python3 train2.py --data_dir ../Chicks_Automatic_Detection_dataset/Processed_Data_10sec/audio_segments --epochs 200  --batch_size 16 --output_dir ./results
 
 
 deactivate
